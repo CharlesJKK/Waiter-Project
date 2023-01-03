d9df6973ce592bb863687ae5fc29a62c88a904c7
Charles Junior <selrahcjunior@hotmail.com>
2023-01-03 18:26:24 -0300

    Inicio do projeto em React Native

---
+++ MobileAPP/App.tsx
@@ -0,0 +1,10 @@
+import Main from "./src/Main";
+
+const App = () => {
+
+	return (
+		<Main/>
+	);
+};
+
+export default App;
---
+++ MobileAPP/src/Main/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "../components/Text";
+import { Container } from "./styles";
+
+
+export default function Main(){
+	return(
+		<Container>
+			<Text weight="Regular" color="white" size={16} opacity={0.8}>oi</Text>
+		</Container>
+	);
+}
